0d75fca0e5cf9ff852c700025ccb32e11e8e26f3
kabartle <kabartle@umich.edu>
2023-10-30 18:50:38 -0400

    Added final edits and comments

--- heat.py
+++ heat.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+'''
+Lab 4: Heat Diffusion and Permafrost
+
+The function, heat_solve(), is where the core algorithm to calculate heat diffusion
+is implemented. The function, test(), validates heat_solve(). The function, 
+permafrost() creates the plots to answer question 2 of the lab and warming() 
+creates the plots to answer question 3 of the lab.
+
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,15 +17,65 @@ def sample_init(x):
     '''Simple inital boundary conition function'''
     return 4*x - 4*x**2
 
-def heat_solve(xmax=1.0, dx=0.2, tmax=0.2, dt=0.02, c2=1.0, init=sample_init, top=0, bottom=0, Neumann=False):
+# Kangerlussuaq average temperature:
+t_kanger = np.array([-19.7, -21.0, -17., -8.4, 2.3, 8.4,
+                     10.7, 8.5, 3.1, -6.0, -12.0, -16.9])
+
+def temp_kanger(t): 
     '''
+    For an array of times in days, return timeseries of temperature 
+    for Kangerlussuaq, Greenland.
+    '''
+    t_amp = (t_kanger - t_kanger.mean()).max()
+    return t_amp*np.sin(np.pi/180 * t - np.pi/2) + t_kanger.mean()
+
+def warm_kanger1(t): 
+    '''
+    For an array of times in days, return timeseries of temperature 
+    for Kangerlussuaq, Greenland with 0.5 degrees warming.
+    '''
+    t_amp = (t_kanger - t_kanger.mean()).max()
+    return t_amp*np.sin(np.pi/180 * t - np.pi/2) + t_kanger.mean() + 0.5
+
+def warm_kanger2(t): 
+    '''
+    For an array of times in days, return timeseries of temperature 
+    for Kangerlussuaq, Greenland with 1 degrees warming.
+    '''
+    t_amp = (t_kanger - t_kanger.mean()).max()
+    return t_amp*np.sin(np.pi/180 * t - np.pi/2) + t_kanger.mean() + 1
+
+def warm_kanger3(t): 
+    '''
+    For an array of times in days, return timeseries of temperature 
+    for Kangerlussuaq, Greenland with 3 degrees warming.
+    '''
+    t_amp = (t_kanger - t_kanger.mean()).max()
+    return t_amp*np.sin(np.pi/180 * t - np.pi/2) + t_kanger.mean() + 3
+
+
+def heat_solve(xmax=1.0, dx=0.2, tmax=0.2, dt=0.02, c2=1.0, init=sample_init, 
+               top=0, bottom=0, Neumann=False):
+    '''
+    This function solves the 1D heat diffusion equation using a forward difference
+    method given the parameters listed below.
+
+    Parameters
+    ----------
+    xmax, tmax : float, default=1.0,0.2
+        Total space and time heat diffusion is calculated for
     dt, dx : float, default=0.02,0.2
         Tima and space step.
     c2 : float
         Thermal diffusivity.
-    init : scalar or function function
+    init : scalar or function, default=sample_init
         Set initial condition. If a function, should take position
         as input and return temperature
+    top, bottom : scalar or function, default=0,0
+        Set boundary conditions. If a function, should take time
+        as input and return temperature
+
+
     Returns
     -------
     x : numpy vector
@@ -26,6 +86,9 @@ def heat_solve(xmax=1.0, dx=0.2, tmax=0.2, dt=0.02, c2=1.0, init=sample_init, to
 
     '''
 
+    # Check stability criterion
+    if (dt > dx**2/(2*c2)):
+        raise ValueError(f'Stability Criterion not met: dt={dt:6.2f}; dx={dx:06.2f}, c2={c2}')
     # set consatnts
     r = c2 * dt/dx**2
 
@@ -54,31 +117,30 @@ def heat_solve(xmax=1.0, dx=0.2, tmax=0.2, dt=0.02, c2=1.0, init=sample_init, to
         temp[-1,:] = bottom
 
     # Solve!
-    if Neumann:
-        # temp[0,0] = temp[1,0]
-        # temp[-1,0] = temp[-2,0]
+    if Neumann: # Neumann solution
         for j in range(1, N):
             for i in range(1, M-1):
                 temp[i,j] = (1-2*r)*temp[i,j-1] + r*(temp[i+1,j-1] + temp[i-1,j-1])
             temp[0,j] = temp[1,j]
             temp[-1,j] = temp[-2,j]
 
-    else:
+    else: # Dirichlet solution
         for j in range(1, N):
             temp[1:-1, j] = (1-2*r)*temp[1:-1,j-1] + r*(temp[2:,j-1] + temp[:-2,j-1])
 
     return x, t, temp
 
-# Get solution using your solver:
-x, time, heat = heat_solve() # Create a figure/axes object
-fig, axes = plt.subplots(1, 1)
-# Create a color map and add a color bar.
-map = axes.pcolor(time, x, heat, cmap='seismic')
-plt.colorbar(map, ax=axes, label='Temperature ($C$)')
-
-
 
 def test():
+    '''
+    This function performs a validation of heat_solve by comparing the solution
+    to an example heat diffusion scenerio with the expected soltuion.
+
+    Returns
+    -------
+    verify : numpy array
+        Difference between expected and actual solution.
+    '''
     solution = np.array([
     [0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],
     [0.640000, 0.480000, 0.400000, 0.320000, 0.260000, 0.210000, 0.170000, 0.137500, 0.111250, 0.090000, 0.072812],
@@ -97,9 +159,14 @@ def test():
     plt.colorbar(map, ax=axes, label='Temperature ($C$)')
     plt.savefig('test.png')
 
-    return heat-solution
+    verify = heat-solution
+    return verify
 
 def boundaries():
+    '''
+    This function creates the plots for the HW assignment comparing 
+    Neumann and Dirichlet boundary conditions
+    '''
     # Dirichlet solution:
     x, time, heat = heat_solve(dx=0.02, dt=0.0002)
 
@@ -125,3 +192,139 @@ def boundaries():
     plt.xlabel('Time (s)')
     plt.ylabel('Distance (m)')
     plt.savefig('Neumann.png')
+
+def permafrost():
+    '''
+    This function creates the plots for Kangerlussuaq's ground temperature,
+    answering question 2 of the lab assignment.
+    '''
+    # Get solution using your solver:
+    x, time, heat = heat_solve(tmax=18250,xmax=100, dt=10, dx=1, c2=0.0216, bottom=5, top=temp_kanger, init=0) 
+    # Create a figure/axes object
+    fig, axes = plt.subplots(1, 1)
+    # Create a color map and add a color bar.
+    map = axes.pcolor(time/360, x, heat, cmap='seismic', vmin=-25, vmax=25)
+    plt.colorbar(map, ax=axes, label='Temperature ($C$)')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.savefig('HeatMap.png')
+    
+    # Set indexing for the final year of results:
+    loc = int(-365) # Final 365 days of the result.
+    # Extract the min values over the final year:
+    winter = heat[:, loc:].min(axis=1)
+    # Extract the max values over the final year:
+    summer = heat[:, loc:].max(axis=1)
+    # Create a temp profile plot:
+    fig, ax2 = plt.subplots(1, 1, figsize=(10, 8))
+    ax2.plot(winter, x, label='Winter')
+    ax2.plot(summer, x, label='Summer', linestyle='--', color='red')
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.grid()
+    plt.savefig('TempProfile.png')
+
+def warming():
+    '''
+    This function creates the plots for Kangerlussuaq's ground temperature
+    with 0.5, 1, and 3 degrees C of uniform temperature rise, answering 
+    question 3 of the lab assignment.
+    '''
+    # Get solution using your solver for 0.5 degrees of warming:
+    x, time, heat = heat_solve(tmax=18250,xmax=100, dt=10, dx=1, c2=0.0216, bottom=5, top=warm_kanger1, init=0) 
+    # Create a figure/axes object
+    fig, axes = plt.subplots(1, 1)
+    # Create a color map and add a color bar.
+    map = axes.pcolor(time/360, x, heat, cmap='seismic', vmin=-25, vmax=25)
+    plt.colorbar(map, ax=axes, label='Temperature ($C$)')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.savefig('HeatMap0.5.png')
+    
+    # Set indexing for the final year of results:
+    loc = int(-365) # Final 365 days of the result.
+    # Extract the min values over the final year:
+    winter = heat[:, loc:].min(axis=1)
+    # Extract the max values over the final year:
+    summer = heat[:, loc:].max(axis=1)
+    # Create a temp profile plot:
+    fig, ax2 = plt.subplots(1, 1, figsize=(10, 8))
+    ax2.plot(winter, x, label='Winter')
+    ax2.plot(summer, x, label='Summer', linestyle='--', color='red')
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.grid()
+    plt.savefig('TempProfile0.5.png')
+
+    # Get solution using your solver for 1 degree of warming:
+    x, time, heat = heat_solve(tmax=18250,xmax=100, dt=10, dx=1, c2=0.0216, bottom=5, top=warm_kanger2, init=0) 
+    # Create a figure/axes object
+    fig, axes = plt.subplots(1, 1)
+    # Create a color map and add a color bar.
+    map = axes.pcolor(time/360, x, heat, cmap='seismic', vmin=-25, vmax=25)
+    plt.colorbar(map, ax=axes, label='Temperature ($C$)')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.savefig('HeatMap1.png')
+    
+    # Set indexing for the final year of results:
+    loc = int(-365) # Final 365 days of the result.
+    # Extract the min values over the final year:
+    winter = heat[:, loc:].min(axis=1)
+    # Extract the max values over the final year:
+    summer = heat[:, loc:].max(axis=1)
+    # Create a temp profile plot:
+    fig, ax2 = plt.subplots(1, 1, figsize=(10, 8))
+    ax2.plot(winter, x, label='Winter')
+    ax2.plot(summer, x, label='Summer', linestyle='--', color='red')
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.grid()
+    plt.savefig('TempProfile1.png')
+
+    # Get solution using your solver for 3 degrees of warming:
+    x, time, heat = heat_solve(tmax=18250,xmax=100, dt=10, dx=1, c2=0.0216, bottom=5, top=warm_kanger3, init=0) 
+    # Create a figure/axes object
+    fig, axes = plt.subplots(1, 1)
+    # Create a color map and add a color bar.
+    map = axes.pcolor(time/360, x, heat, cmap='seismic', vmin=-25, vmax=25)
+    plt.colorbar(map, ax=axes, label='Temperature ($C$)')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.savefig('HeatMap3.png')
+    
+    # Set indexing for the final year of results:
+    loc = int(-365) # Final 365 days of the result.
+    # Extract the min values over the final year:
+    winter = heat[:, loc:].min(axis=1)
+    # Extract the max values over the final year:
+    summer = heat[:, loc:].max(axis=1)
+    # Create a temp profile plot:
+    fig, ax2 = plt.subplots(1, 1, figsize=(10, 8))
+    ax2.plot(winter, x, label='Winter')
+    ax2.plot(summer, x, label='Summer', linestyle='--', color='red')
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Depth (m)')
+    plt.title('Ground Temperature: Kangerlussuaq, Greenland')
+    plt.grid()
+    plt.savefig('TempProfile3.png')
+    
